71bc21b53bd206a753bd62a1d39a610281c557e7
ismailmutafchev <ismailmutafchev@gmail.com>
2023-12-10 22:43:53 +0000

    more toasts

--- src/pages/Calendar/components/ShiftModal.tsx
+++ src/pages/Calendar/components/ShiftModal.tsx
@@ -38,22 +38,6 @@ export default function AddShift({ data }: any) {
   const formErrors = formState.errors;
 
   function submit(data: any) {
-    // const employeeBusy = shifts?.some((shift: any) => {
-    //   if (update && shift.id === id) return false;
-    //   return (
-    //     shift.employeeId === data.employee &&
-    //     ((new Date(shift.start) < new Date(data.date + "T" + data.start) &&
-    //       new Date(shift.end) > new Date(data.date + "T" + data.start)) ||
-    //       (new Date(shift.start) < new Date(data.date + "T" + data.end) &&
-    //         new Date(shift.end) > new Date(data.date + "T" + data.end)))
-    //   );
-    // });
-
-    // if (employeeBusy) {
-    //   alert("Employee is busy");
-    //   return;
-    // }
-
     const shiftLength = (
       differenceInMinutes(
         new Date(data.date + "T" + data.end),
