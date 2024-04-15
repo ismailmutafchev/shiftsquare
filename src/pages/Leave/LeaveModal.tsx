cdcf9b4379625af013bf91aec98c8d97c8fde1cd
ismailmutafchev <ismailmutafchev@gmail.com>
2024-04-15 18:45:03 +0100

    update and remove leave request

--- src/pages/Leave/LeaveModal.tsx
+++ src/pages/Leave/LeaveModal.tsx
@@ -1,13 +1,17 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { useForm } from "react-hook-form";
 import { getLeaveAll, getLeaveTypes } from "../../queries/leave/queries";
-import { insertLeave, updateLeaveOne } from "../../queries/leave/mutations";
+import {
+  deleteLeaveOne,
+  insertLeave,
+  updateLeaveOne,
+} from "../../queries/leave/mutations";
 import { LoadingAnimation } from "../../assets/AnimationComponents/AnimationComponents";
 import { intervalToDuration } from "date-fns";
 
 type LeaveProps = {
-  start: Date;
-  end: Date;
+  start: Date | string;
+  end: Date | string;
   details: string;
   type: "holiday" | "sickLeave" | "unpaidLeave" | "maternity" | "paternity";
   status: "pending" | "approved" | "rejected";
@@ -15,13 +19,25 @@ type LeaveProps = {
 
 export function LeaveRequest({ data }: any) {
   const update = data.isUpdate;
-  const id = data.id;
+  const id = data.data.id;
   const { modalHandler } = data;
 
-  const { handleSubmit, register } = useForm<LeaveProps>();
+  const { data: leaveTypesData, loading } = useQuery(getLeaveTypes);
+
+  const leaveTypes = leaveTypesData?.leave_type;
+
+  const { handleSubmit, register } = useForm<LeaveProps>({
+    defaultValues: {
+      start: new Date(data.data.start).toISOString().split("T")[0],
+      end: new Date(data.data.end).toISOString().split("T")[0],
+      details: data.data.details,
+      type: data.data.type,
+    },
+  });
 
   const [addLeave] = useMutation(insertLeave);
   const [updateLeave] = useMutation(updateLeaveOne);
+  const [removeLeave] = useMutation(deleteLeaveOne);
 
   function submit(data: LeaveProps) {
     const duration = intervalToDuration({
@@ -54,9 +70,16 @@ export function LeaveRequest({ data }: any) {
       onCompleted: () => modalHandler(false),
     });
   }
-  const { data: leaveTypesData, loading } = useQuery(getLeaveTypes);
 
-  const leaveTypes = leaveTypesData?.leave_type;
+  function removeLeaveOne(id: string) {
+    removeLeave({
+      variables: {
+        id: id,
+      },
+      refetchQueries: [{ query: getLeaveAll }],
+      onCompleted: () => modalHandler(false),
+    });
+  }
 
   if (loading)
     return (
@@ -138,14 +161,24 @@ export function LeaveRequest({ data }: any) {
           </div>
         </div>
       </div>
-
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button
-          type="submit"
-          className="inline-flex items-center rounded-md bg-polar-800/90 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-200 hover:text-polar-800/90 hover:ring-1 ring-polar-800/90  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-polar-800/90"
-        >
-          Submit
-        </button>
+      <div className="w-full flex justify-between">
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            onClick={() => removeLeaveOne(id)}
+            className="inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-200 hover:text-gray-900 hover:ring-1 ring-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300"
+          >
+            Remove Request
+          </button>
+        </div>
+        <div className="mt-6 flex items-ceßnter justify-end gap-x-6">
+          <button
+            type="submit"
+            className="inline-flex items-center rounded-md bg-polar-800/90 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-200 hover:text-polar-800/90 hover:ring-1 ring-polar-800/90  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-polar-800/90"
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </form>
   );
