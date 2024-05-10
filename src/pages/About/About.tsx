f8fee88d7fa866ca9f9e161688a8e9dcf5e73f51
alendarov92 <104994198+alendarov92@users.noreply.github.com>
2024-05-10 18:15:36 +0100

    about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -157,7 +157,7 @@ const footerNavigation = {
 export default function Example() {
 
   return (
-    <div className="bg-gray-900">
+    <div className="bg-white">
 
       <main className="relative isolate">
         {/* Background */}
@@ -177,7 +177,7 @@ export default function Example() {
         {/* Header section */}
         <div className="px-6 pt-14 lg:px-8">
           <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
-            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">We love creators</h2>
+            <h2 className="text-4xl font-bold tracking-tight text-gray-800 sm:text-6xl">We love creators</h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
               fugiat veniam occaecat fugiat aliqua.
