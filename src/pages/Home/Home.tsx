44dc4a6873d1b468fbbaf5abc076ddb383e402b0
ismailmutafchev <ismailmutafchev@gmail.com>
2024-08-02 21:01:06 +0100

    stripe hosted page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -204,36 +204,21 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <button
-          className="rounded-md hover:cursor-pointer shadow-sm bg-polar-500 px-3.5 py-2.5 text-sm font-semibold text-white hover:bg-polar-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-polar-800"
-          onClick={() =>
-            fetch(
-              "https://7vxuwb2bf6ifaow6bypl7hwikm0bcjeu.lambda-url.eu-west-2.on.aws/",
-              {
-                method: "POST",
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  priceId: "price_1PjPOaE7LxujRcaNb5gXVps3",
-                }),
-              }
-            )
-              .then((res) => {
-                console.log(res);
-                return res.json();
-              })
-              .then((data) => {
-                console.log({ data });
-                return data.clientSecret;
-              })
-              .catch((error) => {
-                console.error("Error:", error);
-              })
-          }
-        >
-          Get Started
-        </button>
+        <section>
+          <div className="product">
+            <img
+              src="https://i.imgur.com/EHyR2nP.png"
+              alt="The cover of Stubborn Attachments"
+            />
+            <div className="description">
+              <h3>Stubborn Attachments</h3>
+              <h5>$20.00</h5>
+            </div>
+          </div>
+          <form action="https://7vxuwb2bf6ifaow6bypl7hwikm0bcjeu.lambda-url.eu-west-2.on.aws/" method="POST" >
+            <button type="submit">Checkout</button>
+          </form>
+        </section>
       </main>
     </div>
   );
